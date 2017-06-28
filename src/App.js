79be19ae49c3c1a6c1eaeba86dfc1d35b273639e
Travis Kaufman <travis.kaufman@gmail.com>
2017-06-28 17:26:03 -0400

    Refactor the App component into a pure function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import 'moment-duration-format';
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import styled from 'styled-components';
 
@@ -44,62 +44,58 @@ const BigTime = styled.time`
   font-weight: 700;
 `;
 
-class App extends Component {
-  static get propTypes() {
-    return {
-      status: PropTypes.string.isRequired,
-      numRounds: PropTypes.number.isRequired,
-      currentRound: PropTypes.number.isRequired,
-      timeRemainingFormatted: PropTypes.string.isRequired,
-      warning: PropTypes.bool.isRequired,
-      onStart: PropTypes.func.isRequired,
-      onPause: PropTypes.func.isRequired,
-      onResume: PropTypes.func.isRequired,
-      onEnd: PropTypes.func.isRequired
-    };
-  }
+const App = ({
+  status,
+  numRounds,
+  currentRound,
+  timeRemainingFormatted,
+  warning,
+  onStart,
+  onPause,
+  onResume,
+  onEnd
+}) => (
+  <div>
+    <div hidden={status !== Status.IDLE}>
+      <Button onClick={onStart}>Start</Button>
+    </div>
+    <div hidden={status === Status.IDLE}>
+      <StatusHeader warning={warning}>{statusText(status, numRounds, currentRound)}</StatusHeader>
+      <BigTime>{timeRemainingFormatted}</BigTime>
+      <ButtonBar>
+        <Button onClick={status === Status.PAUSED ? onResume : onPause}>
+          {status === Status.PAUSED ? 'Resume' : 'Pause'}
+        </Button>
+        <Button onClick={onEnd}>
+          End
+          </Button>
+      </ButtonBar>
+    </div>
+  </div>
+);
+App.propTypes = {
+  status: PropTypes.string.isRequired,
+  numRounds: PropTypes.number.isRequired,
+  currentRound: PropTypes.number.isRequired,
+  timeRemainingFormatted: PropTypes.string.isRequired,
+  warning: PropTypes.bool.isRequired,
+  onStart: PropTypes.func.isRequired,
+  onPause: PropTypes.func.isRequired,
+  onResume: PropTypes.func.isRequired,
+  onEnd: PropTypes.func.isRequired
+};
 
-  constructor(props) {
-    super(props);
-    this.handleStart = () => this.props.onStart();
-    this.handlePause = () => this.props.onPause();
-    this.handleResume = () => this.props.onResume();
-  }
-
-  render() {
-    let statusText = `Round ${this.props.currentRound} / ${this.props.numRounds}`;
-    switch (this.props.status) {
-      case Status.COUNTDOWN:
-        statusText = 'Get Ready!';
-        break;
-      case Status.PAUSED:
-        statusText += ' (Paused)';
-        break;
-      case Status.REST:
-        statusText = 'Rest';
-        break;
-      default:
-        break;
-    }
-    return (
-      <div>
-        <div hidden={this.props.status !== Status.IDLE}>
-          <Button onClick={this.handleStart}>Start</Button>
-        </div>
-        <div hidden={this.props.status === Status.IDLE}>
-          <StatusHeader warning={this.props.warning}>{statusText}</StatusHeader>
-          <BigTime>{this.props.timeRemainingFormatted}</BigTime>
-          <ButtonBar>
-            <Button onClick={this.props.status === Status.PAUSED ? this.handleResume : this.handlePause}>
-              {this.props.status === Status.PAUSED ? 'Resume' : 'Pause'}
-            </Button>
-            <Button onClick={this.props.onEnd}>
-              End
-            </Button>
-          </ButtonBar>
-        </div>
-      </div>
-    );
+function statusText(status, numRounds, currentRound) {
+  const statusText = `Round ${currentRound} / ${numRounds}`;
+  switch (status) {
+    case Status.COUNTDOWN:
+      return 'Get Ready!';
+    case Status.PAUSED:
+      return `(Paused)`;
+    case Status.REST:
+      return 'Rest';
+    default:
+      return statusText;
   }
 }
 
